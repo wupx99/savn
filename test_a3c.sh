8f112770c4f6820dae66aeb3947ee97956b06e01
wupx <3344681919@qq.com>
2020-12-16 19:21:43 +0800

    modify

---
+++ test_a3c.sh
@@ -0,0 +1,3 @@
+python main.py --eval --test_or_val test --episode_type TestValEpisode --load_model /data/wpx/pretrained_models/nonadaptivea3c_pretrained.dat --results_json nonadaptivea3c_test.json
+
+cat nonadaptivea3c_test.json
---
+++ train_savn.sh
@@ -0,0 +1,5 @@
+python main.py \
+--title savn_train \
+--model SAVN \
+--gpu-ids 0 1 \
+--workers 12
